e7932b7ed5da77df0f74c0c81fb4227a362578b6
Kevin Wei <KevinWeiHadExtra>
2021-02-23 17:10:47 -0800

    Update Mystuff

---
+++ Testing/Assets/MyScripts/BoxController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+    private GameObject _ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_ball==null && Input.GetMouseButtonDown(0))
+        {
+            Vector3 location = new Vector3(0, 20, 0);
+            _ball = Instantiate(ballPrefab, location, transform.rotation) as GameObject;
+        }
+        if (_ball != null && Input.GetMouseButtonDown(1))
+        {
+            Destroy(_ball);
+        }
+    }
+}
---
+++ Testing/Assets/MyScripts/BoxGravity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+
+public class BoxGravity : MonoBehaviour
+{
+    public float gravity = -9.8f;
+    private CharacterController _charController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(0, 10, 0);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    }
+}
